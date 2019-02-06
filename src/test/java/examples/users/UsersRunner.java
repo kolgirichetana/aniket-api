e0acf8601ec64ccaa081d07fb5bbefd779124f80
malusarea06 <malusarea111@gmail.com>
2019-02-06 10:56:20 +0530

    Azure Report

--- src/test/java/examples/users/UsersRunner.java
+++ src/test/java/examples/users/UsersRunner.java
@@ -1,9 +1,23 @@
 package examples.users;
 
 import com.intuit.karate.junit4.Karate;
+
+import cucumber.api.CucumberOptions;
+import utilities.AzureReport;
+
+import java.net.MalformedURLException;
+
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
+@CucumberOptions(
+		plugin= {"pretty","json:target/cucbumber-html-report/Cucumber.json"})
 public class UsersRunner {
-
+	
+	@BeforeClass
+    public  static void setup() throws MalformedURLException {
+			AzureReport.generateReport();
+    }
+	
 }
---
+++ src/test/java/utilities/AzureReport.java
@@ -0,0 +1,34 @@
+package utilities;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.Reportable;
+
+public class AzureReport {
+
+	public static void generateReport()
+	{
+		File reportOutputDirectory = new File("/Users/aniketmalusare/eclipse-workspace/karatedemo/target/AzureReports/");
+		List<String> jsonFiles = new ArrayList<>();
+		jsonFiles.add("/Users/aniketmalusare/eclipse-workspace/karatedemo/target/cucbumber-html-report/Cucumber.json");
+		
+		String buildNumber = "1";
+		String projectName = "cucumberProject";
+		boolean runWithJenkins = true;
+		
+		Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+		configuration.setRunWithJenkins(runWithJenkins);
+		configuration.setBuildNumber(buildNumber);
+		// addidtional metadata presented on main page
+		configuration.addClassifications("Platform", "Windows");
+		configuration.addClassifications("Browser", "Firefox");
+		configuration.addClassifications("Branch", "release/1.0");
+		ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+		Reportable result = reportBuilder.generateReports();
+	}
+	
+}
